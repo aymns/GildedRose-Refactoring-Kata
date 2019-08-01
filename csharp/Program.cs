bded74335efc14175f048ec4a2c8ce13f003c7b5
Ayman <aymn.s@live.com>
2019-08-01 18:23:52 +0300

    cleanup program.cs

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace csharp
 {
@@ -14,51 +13,17 @@ namespace csharp
             var app = new GildedRose(items);
 
 
-            for (var i = 0; i < 31; i++)
+            for (var day = 0; day < 31; day++)
             {
-                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("-------- day " + day + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < items.Count; j++)
+                foreach (var item in items)
                 {
-                    System.Console.WriteLine(items[j]);
+                    System.Console.WriteLine(item);
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
             }
-        }
-    }
-
-    public class ItemRepository
-    {
-        public IList<Item> GetAll()
-        {
-            return new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-                // this conjured item does not work properly yet
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
         }
     }
 }
