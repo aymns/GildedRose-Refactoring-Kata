dbeebff5a76cfa95137fe68af43961140d261052
Ayman <aymn.s@live.com>
2019-08-01 18:22:51 +0300

    Create item repository

--- csharp/ItemRepository.cs
+++ csharp/ItemRepository.cs
@@ -1,33 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace csharp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("OMGHAI!");
-
-            var items = new ItemRepository().GetAll();
-
-            var app = new GildedRose(items);
-
-
-            for (var i = 0; i < 31; i++)
-            {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < items.Count; j++)
-                {
-                    System.Console.WriteLine(items[j]);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
-            }
-        }
-    }
-
     public class ItemRepository
     {
         public IList<Item> GetAll()
